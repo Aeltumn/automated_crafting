b2db4296ff8ca9dd43f197aed8aef701b5190f15
Daniël Goossens <daniel@goossens.ch>
2020-10-07 13:20:14 +0200

    Update to CustomCrafting v1.6

--- src/main/java/nl/dgoossens/autocraft/compat/CustomCraftingCompat.java
+++ src/main/java/nl/dgoossens/autocraft/compat/CustomCraftingCompat.java
@@ -1,9 +1,13 @@
 package nl.dgoossens.autocraft.compat;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import me.wolfyscript.customcrafting.CustomCrafting;
 import me.wolfyscript.customcrafting.handlers.RecipeHandler;
-import me.wolfyscript.customcrafting.recipes.types.CustomRecipe;
-import me.wolfyscript.customcrafting.recipes.types.ShapelessCraftingRecipe;
+import me.wolfyscript.customcrafting.recipes.types.ICustomRecipe;
 import me.wolfyscript.customcrafting.recipes.types.workbench.ShapedCraftRecipe;
 import me.wolfyscript.customcrafting.recipes.types.workbench.ShapelessCraftRecipe;
 import me.wolfyscript.utilities.api.custom_items.CustomItem;
@@ -11,26 +15,29 @@ import nl.dgoossens.autocraft.api.CompatClass;
 import nl.dgoossens.autocraft.helpers.BukkitRecipe;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 /**
  * Compatibility with:
- * CustomCrafting v1.5 and newer by WolfyScript
+ * CustomCrafting v1.6 and newer by WolfyScript
  */
 public class CustomCraftingCompat implements CompatClass {
     public Set<nl.dgoossens.autocraft.api.CraftingRecipe> load() {
         Set<nl.dgoossens.autocraft.api.CraftingRecipe> loadedRecipes = new HashSet<>();
-        RecipeHandler recipeHandler = CustomCrafting.getRecipeHandler();
-        for(CustomRecipe cr : recipeHandler.getRecipes().values()) {
-            if(cr instanceof ShapedCraftRecipe)
-                loadedRecipes.add(new BukkitRecipe(cr.getCustomResult(), ((ShapedCraftRecipe) cr).getShape(), ((ShapedCraftRecipe) cr).getIngredients()
-                        .entrySet().parallelStream().collect(Collectors.toMap(Map.Entry::getKey, e -> new ArrayList<>(e.getValue())))));
-            else if(cr instanceof ShapelessCraftRecipe) {
-                Map<Character, List<CustomItem>> m = ((ShapelessCraftingRecipe) cr).getIngredients();
-                loadedRecipes.add(new BukkitRecipe(cr.getCustomResult(), m.entrySet().parallelStream().map(f -> (ArrayList<ItemStack>) new ArrayList(f.getValue())).collect(Collectors.toList())));
+        RecipeHandler recipeHandler = CustomCrafting.getInst().getRecipeHandler();
+        for (ICustomRecipe cr : recipeHandler.getRecipes().values()) {
+            if (cr instanceof ShapedCraftRecipe) {
+                Map<Character, List<CustomItem>> ingredients = ((ShapedCraftRecipe) cr).getIngredients();
+                loadedRecipes.add(new BukkitRecipe(cr.getCustomResult().create(), ((ShapedCraftRecipe) cr).getShape(),
+                        ingredients.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, this::parseIntoItemStacks))));
+            } else if (cr instanceof ShapelessCraftRecipe) {
+                Map<Character, List<CustomItem>> m = ((ShapelessCraftRecipe) cr).getIngredients();
+                loadedRecipes.add(new BukkitRecipe(cr.getCustomResult().create(),
+                        m.entrySet().parallelStream().map(this::parseIntoItemStacks).collect(Collectors.toList())));
             }
         }
         return loadedRecipes;
+    }
+
+    private List<ItemStack> parseIntoItemStacks(Map.Entry<Character, List<CustomItem>> customItems) {
+        return customItems.getValue().stream().map(CustomItem::create).collect(Collectors.toList());
     }
 }
